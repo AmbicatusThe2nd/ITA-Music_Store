ed615d02f763940ec4eb38f0183fba43c9eae672
AmbicatusThe2nd <rok99tkalcic@gmail.com>
2023-03-27 14:56:24 +0200

    Implemented get order by id

--- microservices/store_orders_api/src/main/kotlin/com/example/store_orders_api/application/controllers/OrderController.kt
+++ microservices/store_orders_api/src/main/kotlin/com/example/store_orders_api/application/controllers/OrderController.kt
@@ -3,16 +3,38 @@ package com.example.store_orders_api.application.controllers
 import com.example.store_orders_api.domain.models.OrderModel
 import com.example.store_orders_api.domain.services.IOrderService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/orders")
-class OrderController(@Autowired val orderService: IOrderService) {
+class OrderController(@Qualifier("orderDomainService") @Autowired val orderService: IOrderService) {
     @GetMapping("")
     suspend fun getAllOrders(): ResponseEntity<List<OrderModel>> {
-        return ResponseEntity.ok(orderService.getAllAsync())
+        return try {
+            ResponseEntity.ok(orderService.getAllAsync())
+        } catch (e: Exception) {
+            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
+        }
     }
+
+    @GetMapping("/{id}")
+    suspend fun getOrder(@PathVariable id: String): ResponseEntity<OrderModel> {
+        return try {
+            val order: OrderModel? = orderService.getAsync(id)
+            if (order != null) {
+                ResponseEntity.ok(order)
+            } else {
+                ResponseEntity.notFound().build()
+            }
+        } catch (e: Exception) {
+            ResponseEntity.notFound().build()
+        }
+    }
+
 }
