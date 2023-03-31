8127efad43371d3058392fd2e9dcdbd3484ebe53
AmbicatusThe2nd <rok99tkalcic@gmail.com>
2023-03-31 15:41:19 +0200

    Implemented get by id method

--- microservices/store_sensor_api/src/main/kotlin/com/example/sensor_api/domain/repositories/SensorRepository.kt
+++ microservices/store_sensor_api/src/main/kotlin/com/example/sensor_api/domain/repositories/SensorRepository.kt
@@ -1,13 +1,17 @@
 package com.example.sensor_api.domain.repositories
-
 import com.example.sensor_api.domain.models.SensorModel
 import io.quarkus.mongodb.panache.reactive.ReactivePanacheMongoRepository
 import io.smallrye.mutiny.Uni
+import org.bson.types.ObjectId
 import javax.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
 class SensorRepository : ReactivePanacheMongoRepository<SensorModel> {
     fun findAllSensors(): Uni<List<SensorModel>> {
         return listAll()
+    }
+
+    fun findSensorById(id: ObjectId): Uni<SensorModel> {
+        return findById(id)
     }
 }
