ca47436388b9280befb5272dfd27c158d46cdcb5
AmbicatusThe2nd <rok99tkalcic@gmail.com>
2023-04-01 13:53:18 +0200

    Implemented get by id method

--- microservices/store_sensor_api/src/main/kotlin/com/example/sensor_api/application/resources/GuitarSensorResource.kt
+++ microservices/store_sensor_api/src/main/kotlin/com/example/sensor_api/application/resources/GuitarSensorResource.kt
@@ -3,9 +3,12 @@ package com.example.sensor_api.application.resources
 import com.example.sensor_api.domain.models.SensorModel
 import com.example.sensor_api.domain.repositories.SensorRepository
 import io.smallrye.mutiny.Uni
+import org.bson.types.ObjectId
+import java.lang.IllegalArgumentException
 import javax.inject.Inject
 import javax.ws.rs.GET
 import javax.ws.rs.Path
+import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
 import javax.ws.rs.WebApplicationException
 import javax.ws.rs.core.Response
@@ -17,6 +20,22 @@ class GuitarSensorResource @Inject constructor(private val sensorRepository: Sen
     fun getGuitarSensor(): Uni<List<SensorModel>> {
         try {
             return sensorRepository.findAllSensors()
+        }
+        catch(e: Exception) {
+            throw WebApplicationException("Error getting guitar sensor data", e, Response.Status.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces("application/json")
+    fun getSensorDataById(@PathParam("id") id: String): Uni<SensorModel> {
+        try {
+            val objectId = ObjectId(id)
+            return sensorRepository.findSensorById(objectId)
+        }
+        catch (e: IllegalArgumentException) {
+            throw WebApplicationException("Invalid id", e, Response.Status.BAD_REQUEST)
         }
         catch(e: Exception) {
             throw WebApplicationException("Error getting guitar sensor data", e, Response.Status.INTERNAL_SERVER_ERROR)
