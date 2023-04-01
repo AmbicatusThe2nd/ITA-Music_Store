8de19ce28603be3a7e39c43dfbe2060a3030906e
AmbicatusThe2nd <rok99tkalcic@gmail.com>
2023-04-01 15:18:16 +0200

    Implemented delete method

--- microservices/store_sensor_api/src/main/kotlin/com/example/sensor_api/application/resources/GuitarSensorResource.kt
+++ microservices/store_sensor_api/src/main/kotlin/com/example/sensor_api/application/resources/GuitarSensorResource.kt
@@ -6,6 +6,7 @@ import io.smallrye.mutiny.Uni
 import org.bson.types.ObjectId
 import java.lang.IllegalArgumentException
 import javax.inject.Inject
+import javax.ws.rs.DELETE
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
@@ -33,6 +34,22 @@ class GuitarSensorResource @Inject constructor(private val sensorRepository: Sen
         try {
             val objectId = ObjectId(id)
             return sensorRepository.findSensorById(objectId)
+        }
+        catch (e: IllegalArgumentException) {
+            throw WebApplicationException("Invalid id", e, Response.Status.BAD_REQUEST)
+        }
+        catch(e: Exception) {
+            throw WebApplicationException("Error getting guitar sensor data", e, Response.Status.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Produces("application/json")
+    fun deleteSensorDataById(@PathParam("id") id: String) {
+        try {
+            val objectId = ObjectId(id)
+            sensorRepository.deleteSensorById(objectId)
         }
         catch (e: IllegalArgumentException) {
             throw WebApplicationException("Invalid id", e, Response.Status.BAD_REQUEST)
