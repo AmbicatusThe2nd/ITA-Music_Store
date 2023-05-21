041289875dad06fc7e88d3d3d11997ebd59ca37e
AmbicatusThe2nd <rok99tkalcic@gmail.com>
2023-05-21 14:07:18 +0200

    Implemented swagger configuration for authorization

--- microservices/store_items_api/store_items_api/Program.cs
+++ microservices/store_items_api/store_items_api/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using store_items_api.Authentication;
 using store_items_api.Domain.Repositories;
 using store_items_api.Domain.Services;
@@ -15,7 +16,31 @@ builder.Services.AddSingleton<IItemService, ItemDomainService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("ApiKey", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Description = "The API Key to acces the API",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+        Name = "x-api-key",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Scheme = "ApiKeyScheme"
+    });
+    var scheme = new OpenApiSecurityScheme
+    {
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "ApiKey"
+        },
+        In = ParameterLocation.Header
+    };
+    var requirement = new OpenApiSecurityRequirement
+    {
+        { scheme, new List<string>() }
+    };
+    c.AddSecurityRequirement(requirement);
+});
 
 var app = builder.Build();
 
