b31c99ebd3a76a9c67aa3fe3819e437b8b0a5000
AmbicatusThe2nd <rok99tkalcic@gmail.com>
2023-05-21 15:14:38 +0200

    Implemented circuit breaker in the Gateway

--- gateways/web_gateway/src/services/OrderService.ts
+++ gateways/web_gateway/src/services/OrderService.ts
@@ -1,9 +1,26 @@
 import axios from "axios";
 import Order from "../models/Order";
+import CircuitBreaker from "opossum";
 
 // eslint-disable-next-line max-len
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-var-requires
 require("dotenv").config();
+
+const circuitBreakerOptions = {
+  timeout: 5000,
+  errorThresholdPercentage: 50,
+  resetTimeout: 10000,
+};
+
+const getAllOrdersCircuitBreaker = new CircuitBreaker(
+  getAllOrders,
+  circuitBreakerOptions
+);
+
+const addOrderCircuitBreaker = new CircuitBreaker(
+  addOrder,
+  circuitBreakerOptions
+);
 
 async function getAllOrders(): Promise<Order[]> {
   // eslint-disable-next-line node/no-process-env
@@ -22,4 +39,7 @@ async function addOrder(order: Order): Promise<Order> {
   return response.data;
 }
 
-export default { getAllOrders, addOrder } as const;
+export default {
+  getAllOrders: () => getAllOrdersCircuitBreaker.fire(),
+  addOrder: (order: Order) => addOrderCircuitBreaker.fire(order),
+};
