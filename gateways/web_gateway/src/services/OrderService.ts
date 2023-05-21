ea4d06ff2205a79683f6613405b9823778713351
AmbicatusThe2nd <rok99tkalcic@gmail.com>
2023-05-21 14:49:32 +0200

    Fixed some things

--- gateways/web_gateway/src/services/OrderService.ts
+++ gateways/web_gateway/src/services/OrderService.ts
@@ -1,9 +1,12 @@
 import axios from "axios";
 import Order from "../models/Order";
 
+// eslint-disable-next-line max-len
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-var-requires
 require("dotenv").config();
 
 async function getAllOrders(): Promise<Order[]> {
+  // eslint-disable-next-line node/no-process-env
   const response = await axios.get(process.env.ORDER_SERVICE_URL as string);
   // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return response.data;
@@ -11,6 +14,7 @@ async function getAllOrders(): Promise<Order[]> {
 
 async function addOrder(order: Order): Promise<Order> {
   const response = await axios.post(
+    // eslint-disable-next-line node/no-process-env
     process.env.ORDER_SERVICE_URL as string,
     order
   );
